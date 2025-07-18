b2aa8789ae61df1acd09311fdc5fd80226ab574f
septiandwica <septiandwica03@gmail.com>
2025-07-18 10:19:29 +0700

    update use react router dom

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => (
   <div className=" border-t border-orange-100 w-full">
@@ -16,15 +17,15 @@ const Footer: React.FC = () => (
           <div className="text-amber-700 text-sm">+62 812-3456-7890</div>
         </div>
         <div className="flex-1 min-w-[200px] flex flex-col gap-2 text-amber-800 font-medium mb-6 md:mb-0">
-          <a href="/about" className="hover:text-orange-600 transition">
+          <Link to="/about" className="hover:text-orange-600 transition">
             About Us
-          </a>
-          <a href="/contact" className="hover:text-orange-600 transition">
+          </Link>
+          <Link to="/contact" className="hover:text-orange-600 transition">
             Contact Us
-          </a>
-          <a href="/product" className="hover:text-orange-600 transition">
+          </Link>
+          <Link to="/product" className="hover:text-orange-600 transition">
             Product
-          </a>
+          </Link>
         </div>
         <div className="flex-1 min-w-[200px] flex flex-col items-start md:items-end gap-2">
           <div className="font-semibold text-orange-800 mb-2">
@@ -32,7 +33,7 @@ const Footer: React.FC = () => (
           </div>
           <div className="flex gap-3">
             <a
-              href="https://instagram.com/buevnbakehouse"
+              href="https://instagram.com/buevn.co"
               target="_blank"
               rel="noopener noreferrer"
               className="w-9 h-9 rounded-full bg-orange-100 flex items-center justify-center hover:bg-orange-200 transition"
